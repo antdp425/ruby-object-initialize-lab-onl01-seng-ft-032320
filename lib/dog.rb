ca806c59cddea95c644a1db5490e063fe6a6164f
Anthony DiPietrantonio <antdp425@aol.com>
2020-03-30 10:32:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,7 @@
 class Dog
 att_accessor:name,breed
-def initialize(:name,:breed="Mutt")
-  @name = name
-  @breed = breed
-end
-
-
+  def initialize(:name,:breed="Mutt")
+    @name = name
+    @breed = breed
+  end
 end
